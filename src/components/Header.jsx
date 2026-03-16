690e3735c515790f7138b1deb16e895334057bba
Alvar Sjögren <alvar.m.sjogren@gmail.com>
2026-03-16 19:47:11 +0100

    Added the Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/header.css'
+
+function Header() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+    <header>
+      <nav className={menuOpen ? 'nav-open' : ''}>
+        <Link to="/" className="logo">
+          <div className="logo-svg"></div>
+          <span>Björnsmide</span>
+        </Link>
+        <button className="nav-toggle-label" onClick={() => setMenuOpen(!menuOpen)}>
+          <span></span>
+        </button>
+        <ul>
+          <li><Link to="/produkter">Produkter</Link></li>
+          <li><Link to="/kontakt">Kontakt</Link></li>
+          <li><Link to="/om-oss">Om oss</Link></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
